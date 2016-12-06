03d5605993e9e9684421eaaf3fc2b30b072d37be
Justo6 <mikejusto6@gmail.com>
2016-12-05 18:08:17 -0800

    stuck

--- JsNew.js
+++ JsNew.js
@@ -1,28 +1,31 @@
-/**
- * Created by Justo on 11/29/16.
- */
-//callback function defined
-function callback(type, value, item) {
-    $('#display').html(value);
-}
-// my_calculator - creates a new calculator object
+$(document).ready(function(){
 
-var my_calculator =  new calculator(callback);
-
-//after DOM load add click handlers to all buttons
-
-$(document).ready(function () {
-    $('button').on('click', function () {
-        console.log("button clicked");
-        var val = $(this).text();
-
-        switch (val) {
-            case 'C':
-                my_calculator.allClear();
-                break;
-            default:
-                my_calculator.addItem($(this).text());
-                break;
-        }
+    $("button").click(function(){
+        $(this).text();
+        console.log($(this).text());
+        var value = $(this).text();
     });
-})
+
+});
+
+
+var elementHolder = [];
+
+function buttonsClicked() {
+    var last_index = elementHolder.length - 1;
+    if (!isNaN(elementHolder[last_index])){
+        elementHolder += elementHolder[last_index]
+    }
+    else{
+        elementHolder.push(elementHolder[last_index])
+    }
+}
+
+
+
+
+
+
+
+
+
