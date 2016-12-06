41fe454021e1940f40f74fcd80ac045af51feb3c
Justo6 <mikejusto6@gmail.com>
2016-12-06 15:39:32 -0800

    basic calculations working

--- JsNew.js
+++ JsNew.js
@@ -1,25 +1,79 @@
 $(document).ready(function(){
-
-    $("button").click(function(){
-        $(this).text();
-        console.log($(this).text());
-        var value = $(this).text();
-    });
+    $(".numberButton").click(numberPressed);
+    $(".operatorButton").click(operatorPressed);
+    $(".equalsButton").click(processData);
+    $("#c").click(clearDisplay);
 
 });
+var elementHolder = [""];
+var elementIndex = 0;
 
+function numberPressed () {
+    elementHolder[elementIndex] += $(this).text();
+    displayData();
+}
+function operatorPressed () {
+    elementIndex+=1;
+    elementHolder[elementIndex] = ($(this).text());
+    elementIndex+=1;
+    elementHolder[elementIndex] ='';
+    displayData();
+}
+function displayData(){
+    var output = elementHolder.join('');
+    $('#display').val(output);
+}
+function processData(){
+    var num1 = parseInt(elementHolder[0]);
+    var operator =(elementHolder[1]);
+    var num2 = parseInt(elementHolder[2]);
+    var result = doMath(num1,num2, operator);
+    elementHolder.splice(1,2);
+    elementHolder[0]=result;
+    elementIndex = elementHolder.length;
+    displayData();
+}
 
-var elementHolder = [];
+function doMath(num1, num2, operator) {
 
-function buttonsClicked() {
-    var last_index = elementHolder.length - 1;
-    if (!isNaN(elementHolder[last_index])){
-        elementHolder += elementHolder[last_index]
+    if(operator ===  "+" ){
+        return (num1 + num2);
+    }
+    else if (operator === "-"){
+        return (num1 - num2);
+    }
+    else if (operator === '*'|| operator === "x"){
+        return (num1 * num2);
+    }
+    else if (operator === '/'){
+        return (num1 / num2);
     }
     else{
-        elementHolder.push(elementHolder[last_index])
+        return null;
+
     }
+
 }
+
+function clearDisplay (){
+    elementHolder.splice(0,elementHolder.length);
+    elementHolder = [""];
+    elementIndex = 0;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
