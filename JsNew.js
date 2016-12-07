447272f991ea328c42934b003038c1e95146823a
Justo6 <mikejusto6@gmail.com>
2016-12-07 14:15:04 -0800

    section 2

--- JsNew.js
+++ JsNew.js
@@ -2,45 +2,63 @@ $(document).ready(function(){
     $(".numberButton").click(numberPressed);
     $(".operatorButton").click(operatorPressed);
     $(".equalsButton").click(processData);
-    $(".decimal").click(decimalPressed);
     $("#c").click(clearDisplay);
-
+    $(".decimal").click(decimalPressed);
 });
+
 var elementHolder = [""];
 var elementIndex = 0;
+var decimalButtonPressed =false;
 
 function numberPressed () {
     elementHolder[elementIndex] += $(this).text();
     displayData();
 }
+
 function operatorPressed () {
+    if(elementHolder[elementIndex] === '' && elementHolder.length === 3){
+        elementHolder.splice(elementIndex-1,2);
+        elementIndex = elementIndex -2 ;
+    }
     elementIndex+=1;
     elementHolder[elementIndex] = ($(this).text());
     elementIndex+=1;
     elementHolder[elementIndex] ='';
-    console.log(elementHolder);
-    while (elementHolder.length > 3 ){
-        processData();
-    }
+    decimalButtonPressed = false;
     displayData();
 }
+
+function decimalPressed () {
+    if(decimalButtonPressed ){
+       return;
+    }else {
+        elementHolder[elementIndex] += $(this).text();
+        displayData();
+        decimalButtonPressed = true;
+    }
+}
+
 function displayData(){
-    var output = elementHolder.join('');
+    var output = elementHolder.join(' ');
     $('#display').val(output);
 }
-function processData(){
-    var num1 = parseFloat(elementHolder[0]);
-    var operator =(elementHolder[1]);
-    var num2 = parseFloat(elementHolder[2]);
-    var result = doMath(num1,num2, operator);
-    elementHolder.splice(1,2);
-    elementHolder[0]=result;
-    elementIndex = elementHolder.length - 1 ;
+
+function processData() {
+    while (elementHolder.length >= 3 && elementHolder[2]!='') {
+        var num1 = parseFloat(elementHolder[0]);
+        var operator = (elementHolder[1]);
+        var num2 = parseFloat(elementHolder[2]);
+        var result = doMath(num1, num2, operator);
+        elementHolder.splice(1, 2);
+        elementHolder[0] = result;
+        elementIndex = elementHolder.length - 1;
+    }
     displayData();
+    decimalButtonPressed = true;
+
 }
 
 function doMath(num1, num2, operator) {
-
     if(operator ===  "+" ){
         return (num1 + num2);
     }
@@ -51,29 +69,23 @@ function doMath(num1, num2, operator) {
         return (num1 * num2);
     }
     else if (operator === '/'){
-        return (num1 / num2);
+        return (num1 /num2);
     }
     else{
         return null;
-
     }
-
-}
-
-function decimalPressed () {
-    elementHolder += $(this).text();
-    displayData();
-
-
-
 }
 
 function clearDisplay (){
     elementHolder.splice(0,elementHolder.length);
     elementHolder = [""];
     elementIndex = 0;
+    var output = elementHolder.join(' ');
+    $('#display').val(output);
     
 }
+
+
 
 
 
