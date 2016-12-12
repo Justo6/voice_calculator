3364fc195d9116a325a33685df1d6a96c77522e2
Justo6 <mikejusto6@gmail.com>
2016-12-11 17:42:27 -0800

    get to function with one number and one operator

--- JsNew.js
+++ JsNew.js
@@ -10,6 +10,7 @@ var elementHolder = [""];
 var elementIndex = 0;
 var decimalButtonPressed =false;
 
+
 function numberPressed () {
     elementHolder[elementIndex] += $(this).text();
     displayData();
@@ -44,7 +45,32 @@ function displayData(){
 }
 
 function processData() {
-    while (elementHolder.length >= 3 && elementHolder[2]!='') {
+    //I don't have 2 operands and an operator to do math
+    if (elementHolder.length <= 3 || elementHolder[2] != '') {
+        //do I have a 1st operand and an operator??
+        if (!isNaN(elementHolder[0]) && (isNaN(elementHolder[1]) && elementHolder[1] != undefined)) {
+            //copy thele 1st operand to the 2nd operand
+            console.log("We have 1st operand and operator");
+            var num1 = parseFloat(elementHolder[0]);
+            var operator = elementHolder[1];
+            var num2 = parseFloat(elementHolder[0]);
+            elementHolder[2]= num2;
+            var result = doMath(num1, num2, operator);
+
+            return result;
+
+        }
+        else {
+            //otherwise, do I have only a 1st operand?
+
+        }
+
+
+        //do I have an operator and 2nd operand from my previous calculation?
+        //copy the operator into the elementHolder array at the appropriate place
+        //copy the 2nd operand into the elementHolder array at the appropriate place
+    } //continue with math
+    while (elementHolder.length >= 3 && elementHolder[2] != '') {
         var num1 = parseFloat(elementHolder[0]);
         var operator = (elementHolder[1]);
         var num2 = parseFloat(elementHolder[2]);
@@ -55,8 +81,8 @@ function processData() {
     }
     displayData();
     decimalButtonPressed = true;
-
 }
+
 
 function doMath(num1, num2, operator) {
     if(operator ===  "+" ){
@@ -84,6 +110,8 @@ function clearDisplay (){
     $('#display').val(output);
     
 }
+
+//
 
 
 
