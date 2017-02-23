460c5ad31b39db85e489acba057d503b52c077f7
Justo6 <mikejusto6@gmail.com>
2017-02-23 15:50:00 -0800

    starting multiple operation keys

--- JsNew.js
+++ JsNew.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $(".numberButton").click(numberPressed);
     $(".operatorButton").click(operatorPressed);
-    $(".equalsButton").click(processData);
+    $(".equalsButton").click(equalPressed);
     $("#c").click(clearDisplay);
     $(".decimal").click(decimalPressed);
 });
@@ -14,22 +14,30 @@ var storage=[];
 function numberPressed () {
     elementHolder[elementIndex] += $(this).text();
     displayData();
+
 }
-
+function equalPressed(){
+    processData($(this).text())
+}
 function operatorPressed () {
-
-    if (elementHolder[elementIndex] === '' || elementHolder.length === 3){
-        processData();
+    elementIndex = 1;
+    if (elementHolder[0] == [""] ){
+        return;
+    }
+    if(elementHolder.length===3) {
+        processData($(this).text());
+    }
+    else{
+        elementHolder[1] = ($(this).text());
     }
 
-
+    if(elementHolder[1] !== undefined){
+        elementHolder[1] = ($(this).text());
+    }
         elementIndex++;
-        elementHolder[elementIndex] = ($(this).text());
-        elementIndex++;
-        elementHolder[elementIndex] ='';
+        elementHolder[2] = '';
         decimalButtonPressed = false;
         displayData();
-
 }
 
 function decimalPressed () {
@@ -47,7 +55,8 @@ function displayData(){
     $('#display').val(output);
 }
 
-function processData() {
+function processData(last_operator) {
+    var last_operator = last_operator;
     //only works if you have 1 operator and a operand
     if (elementHolder.length <= 3 && elementHolder[2] == "") {
         var num1 = parseFloat(elementHolder[0]);
@@ -71,7 +80,22 @@ function processData() {
         return result;
     }
     //basic guide for 2 operators and 1 operand
+
     if (elementHolder.length >= 3 && elementHolder[2] !== '') {
+        if(last_operator == "+" ||last_operator == "-" || last_operator == "/" ||last_operator == "x" ){
+            var num1 = parseFloat(elementHolder[0]);
+            var operator = (elementHolder[1]);
+            var num2 = parseFloat(elementHolder[2]);
+            var result = doMath(num1, num2, operator);
+            storage.push(operator);
+            storage.push(num2);
+            elementHolder[0] = result;
+            elementHolder[1] = last_operator;
+            elementHolder.splice(2, 1);
+            elementIndex = elementHolder.length - 1;
+        }
+    }
+    if(last_operator == "="){
         var num1 = parseFloat(elementHolder[0]);
         var operator = (elementHolder[1]);
         var num2 = parseFloat(elementHolder[2]);
@@ -81,13 +105,14 @@ function processData() {
         elementHolder[0] = result;
         elementHolder.splice(1, 2);
         elementIndex = elementHolder.length - 1;
-
     }
+
     displayData();
     decimalButtonPressed = true;
 }
 
 function doMath(num1, num2, operator) {
+
     if(operator ===  "+" ){
         return (num1 + num2);
     }
